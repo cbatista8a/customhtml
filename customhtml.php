a650b890f4b4ce0a728199ab1c579f8836fc66b1
Carlos Batista <59052959+cbatista8a@users.noreply.github.com>
2024-04-23 21:36:50 +0000

    [fix] - update author name

--- customhtml.php
+++ customhtml.php
@@ -47,7 +47,7 @@ class CustomHtml extends Module implements WidgetInterface
         $this->name = 'customhtml';
         $this->tab = 'administration';
         $this->version = '1.0.1';
-        $this->author = 'Cuba Devops';
+        $this->author = 'CubaDevOps';
         $this->need_instance = 1;
         $this->bootstrap = true;
 
